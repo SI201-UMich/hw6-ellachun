2b7b4b385d0abe16dc25cbc9097c39228c98dd10
Ella Chun <ellachun@MacBook-Pro-1305.local>
2026-03-29 14:53:38 -0400

    started code

--- startercode.py
+++ startercode.py
@@ -1,7 +1,7 @@
 # SI 201 HW6 (APIs, JSON, and Caching)
-# Your name:
-# Your student id:
-# Your email:
+# Your name: Ella Chun
+# Your student id: 15359232
+# Your email: ellachun@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
